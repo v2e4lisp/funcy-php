097e5bc04f961b9143b0237c48dab490f440a0cf
wenjun.yan <mylastnameisyan@gmail.com>
2013-11-03 23:09:22 +0900

    first. map and reduce.

---
+++ core.php
@@ -0,0 +1,17 @@
+<?php
+
+function map($arr, $fn) {
+    $ret = array();
+    foreach ($arr as $key => $val) {
+        $ret[] = $fn($val, $key);
+    }
+    return $ret;
+}
+
+function reduce($arr, $fn, $init=0) {
+    $acc = $init;
+    foreach($arr as $key => $val) {
+        $acc = $fn($acc, $val, $key);
+    }
+    return $acc;
+}
